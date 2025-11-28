210c039f071c1a2bcf3964c938ca1f2c1e187b42
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2025-11-28 18:05:36 +0800

    Create orm config and models

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,20 @@
 # Flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 # SQLALCHEMY
 
 
-app = __name__
-db(app)
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:mayday09@localhost:5432/ds"
+
+db = SQLAlchemy(app)
+
+
+
+
 
 
 # sql alchemy
--- backend/model.py
+++ backend/model.py
@@ -5,9 +5,32 @@
 # function toJson
 # map kv pairs
 
-Class Products:
+from config import db
+
+class Products(db.Model):
+    prod_id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(80))
+    g_name = db.Column(db.String(80))
+    b_name = db.Column(db.String(80))
+    d_arrived = db.Column(db.String(80))
+    d_expired = db.Column(db.String(80))
+    cost = db.Column(db.String(80))
+    price = db.Column(db.String(80))
+    qty = db.Column(db.String(80))
+    stock_status = db.Column(db.String(80))
+    created_at = db.Column(db.String(80))
     
-    
-    
-    def toJson:
-        
+    def to_json(self):
+        return {
+            'prod_id': self.prod_id,
+            'category': self.category,
+            'gName' : self.g_name,
+            'bName': self.b_name,
+            'dArrived': self.d_arrived,
+            'dExpired': self.d_expired,
+            'cost': self.cost,
+            'price': self.price,
+            'qty': self.qty,
+            'stockStatus': self.stock_status,
+            'createdAt': self.created_at
+        }
